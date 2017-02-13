4410093212482fa0c707b3323ef5f6c713ea15cf
Stuart Homfray <stuart.homfray@w3w.cz>
2017-02-13 12:19:09 +0100

    using classes
    - note: main.js contains a fix for the missing 'amortization' table in the tutorial

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-import * as mortgage from './mortgage';
+import Mortgage from './mortgage2';
 
 document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let {monthlyPayment, monthlyRate, amortization} = mortgage.calculateAmortization(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
-    amortization.forEach(month => console.log(month));
+    let table = document.createElement('table');
+    let mortgage = new Mortgage(principal, years, rate);
+    document.getElementById("monthlyPayment").innerHTML = mortgage.monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (rate / 12).toFixed(2);
+    let html = "";
+    table.id = "amortization";
+
+    mortgage.amortization.forEach((year, index) => html += `
+        <tr>
+            <td>${index + 1}</td>
+            <td class="currency">${Math.round(year.principalY)}</td>
+            <td class="stretch">
+                <div class="flex">
+                    <div class="bar principal"
+                         style="flex:${year.principalY};-webkit-flex:${year.principalY}">
+                    </div>
+                    <div class="bar interest"
+                         style="flex:${year.interestY};-webkit-flex:${year.interestY}">
+                    </div>
+                </div>
+            </td>
+            <td class="currency left">${Math.round(year.interestY)}</td>
+            <td class="currency">${Math.round(year.balance)}</td>
+        </tr>
+    `);
+    table.innerHTML = html;
+
+    let oldElem = document.getElementById("amortization");
+    if (!oldElem) {
+        document.querySelector('.content').appendChild(table);
+    } else {
+        oldElem.parentNode.replaceChild(table, oldElem);
+    }
 });
